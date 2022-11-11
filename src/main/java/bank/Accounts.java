c49cff6374c1878b46cc5e56ce561700256b13e5
VenuMadhavPendurthi <117030836+VenuMadhavPendurthi@users.noreply.github.com>
2022-11-11 02:19:17 +0000

    Added Accounts class with getter - setter methods

---
+++ src/main/java/bank/Accounts.java
@@ -0,0 +1,40 @@
+package bank;
+
+public class Accounts
+{
+  private int Id;
+  private String type;
+  private double balance;
+}
+
+public Accounts(int Id,String type,double balance)
+{
+  setId(Id);
+  setType(type);
+  setBalance(balance);
+
+}
+
+  public int getId() {
+    return this.Id;
+  }
+
+  public void setId(int Id) {
+    this.Id = Id;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
